5e7ed1e5ba49b2c398d3ab5f44f8bf10fb0279a5
user128830 <user128830@ny212.local>
2017-08-17 10:12:23 -0700

    update

--- UIKit Exploring/ViewController.swift
+++ UIKit Exploring/ViewController.swift
@@ -9,16 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var toggle: UISwitch!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // making connection between function and property programmatically
+        button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
     }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        if toggle.isOn {
+            print("ON")
+        } else {
+            print("OFF")
+        }
+        print("The slider is set to \(slider.value)")
+        print("Button is tapped")
+        
+    }
+    
+    @IBAction func switchToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            print("Switch is ON")
+        } else {
+            print("Switch is OFF")
+        }
+        
+    }
+    
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        print("The current value is \(sender.value)")
+    }
+    
+    @IBAction func keyboardReturnKeyTapped(_ sender: UITextField) {
+        if let text = sender.text {
+            print(text)
+        }
+    }
+    
+    @IBAction func textChanged(_ sender: UITextField) {
+        if let text = sender.text {
+            print(text)
+        }
+    }
+    
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
